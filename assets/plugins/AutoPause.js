03d9b466f083fb4c5f2b74fe5363d64059c5af91
Freddy Monroy <fredydeltoro@gmail.com>
2020-06-06 20:22:15 -0500

    Use visibilitychange event to pause video when tab is changed

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.hadleVisibilityChange = this.hadleVisibilityChange.bind(this);
   }
   run(player) {
     this.player = player;
@@ -11,12 +12,23 @@ class AutoPause {
     });
 
     observer.observe(this.player.media);
+    document.addEventListener('visibilitychange', this.hadleVisibilityChange);
   }
 
   handleIntersection(entries) {
     const entry = entries[0];
 
     if (entry.intersectionRatio >= this.threshold) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+
+  hadleVisibilityChange() {
+    const isVisible = document.visibilityState === 'visible';
+
+    if (isVisible) {
       this.player.play();
     } else {
       this.player.pause();
